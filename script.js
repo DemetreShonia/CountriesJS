bfd7b77a74cc3a1815c5abf5d1945d099dad52ad
DemetreShonia <shonka.demeloper@gmail.com>
2022-08-28 19:25:44 +0400

    Georgia is now Europe ;D

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const renderCountry = function (data, className = '') {
   <img class="country__img" src="${data.flag}" />
   <div class="country__data">
     <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
+    <h4 class="country__region">${
+      data.name == 'Georgia' ? 'EUROPE' : data.region
+    }</h4>
     <p class="country__row"><span>👫</span>${(
       +data.population / 1000000
     ).toFixed(1)}</p>
